b384144e8b6a2001139b2ea59ba0c336639e9b5c
Sugimoto <Sugimoto@DESKTOP-M3D3GRT>
2022-09-27 10:37:45 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp35/Effects.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    abstract class Effect
+    {
+        public Effect(string n)
+        {
+            this.Name = n;
+        }
+        public string Name { get; protected set; }
+        public abstract string GetEffect();
+    }
+    class MagicHeal : Effect
+    {
+        public MagicHeal() : base("Magic Heal")
+        { }
+        public override string GetEffect()
+        {
+            return "Magic Heal cast";
+        }
+    }
+    class MagicalBurning : Effect
+    {
+        public MagicalBurning()
+            : base("Magical Burning")
+        { }
+        public override string GetEffect()
+        {
+            return "Magical Burning cast";
+        }
+    }
+    
+    abstract class EffectDecorator : Effect
+    {
+        protected Effect effect;
+        public EffectDecorator(string n, Effect effect) : base(n)
+        {
+            this.effect = effect;
+        }
+    }
+    class AutoSpell : EffectDecorator
+    {
+        public AutoSpell(Effect p)
+            : base(p.Name + ", Auto Heal", p)
+        { }
+
+        public override string GetEffect()
+        {
+            return effect.GetEffect() + ("healing field");
+        }
+    }
+    class AutoCast : EffectDecorator
+    {
+        public AutoCast(Effect p)
+            : base(p.Name + ", Automatical Cast", p)
+        { }
+
+        public override string GetEffect()
+        {
+            return effect.GetEffect() + ("Fire Burning");
+        }
+    }
+}
---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Effect effect1 = new MagicHeal();
+            effect1 = new AutoSpell(effect1); //Автоматическое исцеление
+            Console.WriteLine("Name: {0}", effect1.Name);
+            Console.WriteLine("Effect: {0}", effect1.GetEffect());
+
+            Effect effect2 = new MagicalBurning();
+            effect2 = new AutoCast(effect2); //Автоматическое исцеление
+            Console.WriteLine("Name: {0}", effect2.Name);
+            Console.WriteLine("Effect: {0}", effect2.GetEffect());
+
+          
+
+            Console.ReadLine();
+        }
+    }
+    
+}
+
